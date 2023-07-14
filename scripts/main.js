ecc7b21f5541d5609ca6806dc31f5f9dc85fb5b0
mambahuang <91522091+mambahuang@users.noreply.github.com>
2023-07-14 13:31:47 +0800

    Team_two intro

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+var myHeading = document.querySelector("#myhead");
+myHeading.textContent = "Hello world!";
+
+let count = 0;
+
+function changeImage(src) {
+    document.querySelector("img").src = "../images/" + src + ".png";
+}
+
+document.body.onmousedown = function () {
+    count += 1;
+    document.querySelector("#pop").textContent = count;
+    changeImage("unko_gorilla");
+}
+document.body.onmouseup = function () {
+    changeImage("gorilla_banana");
+}
